2aa2276392d810cc027c88566f1fac2ee992e615
grizhe <69319142+grizhe@users.noreply.github.com>
2020-12-17 17:46:16 +0000

    Add files via upload
    sadly my desktop is broken, so i dont really have a functioning lab to continue to learn with these tools, and i've been slammed at my part time job with hours but hopefully starting after christmas ill have some time to get back to it : ) this program is just a bunch of comments thats more detailed describing my progress

---
+++ note.py
@@ -0,0 +1,7 @@
+#my computer is currently broken, so sadly I cannot continue, I will be getting
+#parts to fix it soon so hopefully i can continue shortly :')
+#for now i guess i gotta just sit and mess around in kali while i dont have
+#something to view my udemy course in :/
+#i forgot that i should probably post something here as a progress update
+print("why are you running this program lol")
+
